bbf474889b0ad53cb44752a95d0478b88e814143
Skr Phoenix YT <76821526+skrphenix@users.noreply.github.com>
2021-04-11 11:22:22 +0530

    Update aiosqlite-connection.py

--- aiosqlite-connection.py
+++ aiosqlite-connection.py
@@ -13,20 +13,20 @@ file_name = # Enter your file_name which you created (Make sure to crate your fi
 @client.command()
 async def create_table(ctx):
     db = sqlite3.connect(file_name)
-    cursor = db.cursor()
+    cursor = await db.cursor()
     
     cols = ["wallet", "bank"] # You can add as many as columns in this !!!
     
     cursor.execute("""CREATE TABLE economy(userID BIGINT)""")
-    db.commit()
+    await db.commit()
     
     for col in cols:
         cursor.execute(f"ALTER TABLE economy ADD COLUMN {col}")
 
-    db.commit()
+    await db.commit()
 
-    cursor.close()
-    db.close()
+    await cursor.close()
+    await db.close()
 
     await ctx.send("Table created successfully !")
 

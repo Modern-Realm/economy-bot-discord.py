fb44e8670589d1dd496e4e5db7e818be1d75dbcc
Skr Phoenix YT <76821526+skrphenix@users.noreply.github.com>
2021-04-11 11:02:55 +0530

    Update sqlite3-connection.py

--- sqlite3-connection.py
+++ sqlite3-connection.py
@@ -7,20 +7,22 @@ from discord.ext import commands
 import sqlite3
 
 
-file_name = # Enter your file_name which you created (Make sure to crate your file with [.db, .sql, .sqlite3]) !
+file_name = # Enter your file_name which you created (Make sure to crate your file with [.db, .sql, .sqlite3]) , Example: economy.db
 
 # Remove the below code after creating the table !
 @client.command()
 async def create_table(ctx):
     db = sqlite3.connect(file_name)
     cursor = db.cursor()
+    
+    cols = ["wallet", "bank"] # You can add as many as columns in this !!!
+    
+    cursor.execute("""CREATE TABLE economy(userID BIGINT)""")
+    db.commit()
+    
+    for col in cols:
+        cursor.execute(f"ALTER TABLE economy ADD COLUMN {col}")
 
-    cursor.execute("""CREATE TABLE economy(
-      userID BIGINT,
-      wallet INT,
-      bank INT
-    )
-    """)
     db.commit()
 
     cursor.close()

8849353b2b974e3b53da6a0573ee271cbf064468
skrphenix <76821526+skrphenix@users.noreply.github.com>
2023-01-04 20:10:52 +0530

    3.0.1

--- economy with aiosqlite/main.py
+++ economy with aiosqlite/main.py
@@ -8,10 +8,6 @@ from discord.ext import commands
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix=Auth.command_prefix, intents=intents, auto_sync_commands=True)
 
-for file in os.listdir("./cogs"):
-    if file.endswith(".py"):
-        client.load_extension(f"cogs.{file[:-3]}")
-
 
 @client.event
 async def on_ready():
@@ -19,6 +15,17 @@ async def on_ready():
         status=discord.Status.online,
         activity=discord.Game(f"{Auth.command_prefix}help")
     )
+
+    # # if you are using 'discord.py' remove below code
+    for file in os.listdir("./cogs"):
+        if file.endswith(".py"):
+            client.load_extension(f"cogs.{file[:-3]}")
+
+    # if you are using 'discord.py' uncomment(add) below code
+    # for file in os.listdir("./cogs"):
+    #     if file.endswith(".py"):
+    #         await client.load_extension(f"cogs.{file[:-3]}")
+
     print(f"{client.user.name} is online !")
 
 
